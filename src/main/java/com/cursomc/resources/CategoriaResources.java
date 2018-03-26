7346e0b045d16933ca099dab7b77fa9cdd61ec6b
emerson <emersondamiaosouza@gmail.com>
2018-03-26 09:17:41 -0300

    Testando o Dominio

---
+++ src/main/java/com/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,25 @@
+package com.cursomc.resources;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResources {
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+    Categoria cat1=new Categoria(1, "Informatica");
+    Categoria cat2=new Categoria(2, "Roupas");
+    List<Categoria > categorias=new ArrayList<>();
+	categorias.add(cat1);
+	categorias.add(cat2);
+		
+		return categorias;
+	}
+
+}
+
