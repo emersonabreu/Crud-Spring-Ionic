e1698284c5db0ebc60a87367de5be027d0510826
emerson <emersondamiaosouza@gmail.com>
2018-04-21 15:08:32 -0300

    Aula 59: Implementando o toString na Pedido e na ItemPedido

--- src/main/java/com/cursomc/services/PedidoService.java
+++ src/main/java/com/cursomc/services/PedidoService.java
@@ -11,6 +11,7 @@ import com.cursomc.domain.ItemPedido;
 import com.cursomc.domain.PagamentoComBoleto;
 import com.cursomc.domain.Pedido;
 import com.cursomc.domain.enums.EstadoPagamento;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.repositories.ItemPedidoRepository;
 import com.cursomc.repositories.PagamentoRepository;
 import com.cursomc.repositories.PedidoRepository;
@@ -23,83 +24,81 @@ import com.cursomc.services.exceptions.ObjectNotFoundException;
  */
 @Service
 public class PedidoService {
-	
-	
-	
-	//O Spring Injeta o objeto pedidoRepository na classe Pedido PedidoService
+
+	// O Spring Injeta o objeto pedidoRepository na classe Pedido PedidoService
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
-	private ProdutoService produtoService;	
-	
+	private ProdutoService produtoService;
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
+	@Autowired
+	private ClienteService clienteService;
+
 	public Pedido cadastrar(Pedido pedido) {
 		return pedidoRepository.save(pedido);
-		
-		
-			
-		}
-	
+
+	}
+
 	public Pedido insert(Pedido pedido) {
 		pedido.setId(null);
 		pedido.setInstante(new Date());
+		// Como veio só o id do Cliente, ele busca todos os dados desse cliente
+		// no banco e seta no Pedido
+		pedido.setCliente(clienteService.buscaPorId(pedido.getCliente().getId()));
 		pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		pedido.getPagamento().setPedido(pedido);
-		
+
 		if (pedido.getPagamento() instanceof PagamentoComBoleto) {
-			PagamentoComBoleto boleto=(PagamentoComBoleto )pedido.getPagamento();
-			boletoService.preencherPagamentoComBoleto(boleto,pedido.getInstante());
+			PagamentoComBoleto boleto = (PagamentoComBoleto) pedido.getPagamento();
+			boletoService.preencherPagamentoComBoleto(boleto, pedido.getInstante());
 		}
-		
-		pedido=pedidoRepository.save(pedido);
-		
+
+		pedido = pedidoRepository.save(pedido);
+
 		pagamentoRepository.save(pedido.getPagamento());
-		
+
 		for (ItemPedido ip : pedido.getItens()) {
-			
-	  ip.setDesconto(0.0);
-	  ip.setPreco(produtoService.buscaPorId(ip.getProduto().getId()).getPreco());
-	  ip.setPedido(pedido);
+
+			ip.setDesconto(2.0);
+			// Como veio o id do Produto no na lista de ItemPedido
+			// , ele busca todos os dados desse Produto no banco e seta no ItemPedido
+			ip.setProduto(produtoService.buscaPorId(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(pedido);
 		}
-		
+
 		itemPedidoRepository.saveAll(pedido.getItens());
+		System.out.println(pedido);
 		return pedido;
-		
-		
-			
-		}
-	
-	
-	
+
+	}
+
 	public Collection<Pedido> buscaTodos() {
-		
-		
+
 		return pedidoRepository.findAll();
-			
-		}
-	
+
+	}
 
 	/**
-	 *Metodo que busca a Pedido pelo sei id
-	 * Ou se não encontrar o id, lança uma exceção
+	 * Metodo que busca a Pedido pelo sei id Ou se não encontrar o id, lança uma
+	 * exceção
 	 */
-public Pedido buscaPorId(Integer id) {
-	Optional<Pedido> pedido=pedidoRepository.findById(id);
-	
-			return pedido.orElseThrow(()-> new ObjectNotFoundException(
+	public Pedido buscaPorId(Integer id) {
+		Optional<Pedido> pedido = pedidoRepository.findById(id);
+
+		return pedido.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não Encontrado! id:" + id + ", Tipo: " + Pedido.class.getName()));
 
-	 
-}
-	
+	}
 
 }
